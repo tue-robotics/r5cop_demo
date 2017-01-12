7378fe04d06c7a26671609c2b8ba1d57af9521fd
amigo <amigo@amigo1.tue.nl>
2017-01-12 12:26:39 +0100

    feature(): State machine will first grab arms full, then dispose all items

--- src/challenge/amigo/self_cleanup.py
+++ src/challenge/amigo/self_cleanup.py
@@ -31,6 +31,28 @@ class dropPoseDesignator(Designator):
         return ps
 
 
+class ArmFree(smach.State):
+    def __init__(self, robot):
+        smach.State.__init__(self, outcomes=["yes", "no"])
+        self._robot = robot
+
+    def execute(self, userdata):
+	d = UnoccupiedArmDesignator(self._robot.arms, self._robot.rightArm, name="empty_arm_designator2")
+        if d.resolve():
+            return "yes"
+        return "no"
+
+class ArmOccupied(smach.State):
+    def __init__(self, robot):
+        smach.State.__init__(self, outcomes=["yes", "no"])
+        self._robot = robot
+
+    def execute(self, userdata):
+	d = OccupiedArmDesignator(self._robot.arms, self._robot.rightArm, name="empty_arm_designator2")
+        if d.resolve():
+            return "yes"
+        return "no"
+
 class Speak(smach.State):
     def __init__(self, robot, selected_entity_designator, location_id, segment_area):
         smach.State.__init__(self, outcomes=["done"])
@@ -83,7 +105,7 @@ class SelfCleanup(smach.StateMachine):
                                                                   "Let's clean up this object",
                                                                   "Away with this garbage",
                                                                   "Everything will be cleaned"], block=False),
-                                   transitions={"spoken": "PLACE"})
+                                   transitions={"spoken": "CHECK_ARM_FREE"})
 
             smach.StateMachine.add('SAY_GRAB_FAILED',
                                    robot_smach_states.Say(robot, ["I could not grab the item.",
@@ -91,6 +113,10 @@ class SelfCleanup(smach.StateMachine):
                                                                   "I cannot reach the item",
                                                                   "Item grab failed"], block=False),
                                    transitions={"spoken": "failed"})
+
+            smach.StateMachine.add('CHECK_ARM_FREE', ArmFree(robot), transitions={"yes": "done", "no": "CHECK_ARM_OCCUPIED"})
+
+            smach.StateMachine.add('CHECK_ARM_OCCUPIED', ArmOccupied(robot), transitions={"yes": "PLACE", "no": "done"})
 
             smach.StateMachine.add('PLACE',
                                    robot_smach_states.Place(robot, 
@@ -106,10 +132,10 @@ class SelfCleanup(smach.StateMachine):
                                                                   "Yeah!",
                                                                   "Successfully disposed the item",
                                                                   "Another score for AMIGO"], block=False),
-                                   transitions={"spoken": "done"})
+                                   transitions={"spoken": "CHECK_ARM_OCCUPIED"})
 
             smach.StateMachine.add('SAY_PLACE_FAILED',
                                    robot_smach_states.Say(robot, ["I could not cleanup the item.",
                                                                   "I cannot put the item in the trashbin",
                                                                   "Item cleanup failed"], block=False),
-                                   transitions={"spoken": "done"})
+                                   transitions={"spoken": "CHECK_ARM_OCCUPIED"})
